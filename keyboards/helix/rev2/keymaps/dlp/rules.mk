765072d0b961ccc734fdf66eeaace06f450c1aa8
Daniel Luan <daniellun425@gmail.com>
2023-03-04 16:38:10 -0500

    Add helix keymap

---
+++ keyboards/helix/rev2/keymaps/dlp/rules.mk
@@ -0,0 +1,19 @@
+# Set Helix Configs
+HELIX_ROWS = 5              # Helix Rows is 4 or 5
+OLED_ENABLE = no           # OLED_ENABLE
+LOCAL_GLCDFONT = no         # use each keymaps "helixfont.h" insted of "common/glcdfont.c"
+LED_BACK_ENABLE = yes        # LED backlight (Enable WS2812 RGB underlight.)
+LED_UNDERGLOW_ENABLE = no   # LED underglow (Enable WS2812 RGB underlight.)
+LED_ANIMATIONS = no        # LED animations
+IOS_DEVICE_ENABLE = no      # connect to IOS device (iPad,iPhone)
+
+LTO_ENABLE = yes
+
+
+OLED_TIMEOUT = 20000
+
+EXTRAKEY_ENABLE = yes
+WPM_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+
+RAW_ENABLE = yes
