897b00b9ff51da6f5d48fd5d1838d86e90fa988f
Daniel Luan <danielluan425@gmail.com>
2024-08-05 23:27:26 -0700

    fix delete key

--- keyboards/ymdk/id75/keymaps/dlp/keymap.c
+++ keyboards/ymdk/id75/keymaps/dlp/keymap.c
@@ -8,9 +8,11 @@ enum layer_number {
 };
 
 // Layers 
+// https://docs.qmk.fm/features/tri_layer
 #define LOWER    TL_LOWR
-#define SPC_LWR  LT(_LOWER, KC_SPC)
 #define RAISE    TL_UPPR
+// https://docs.qmk.fm/feature_layers#switching-and-toggling-layers
+#define SPC_LWR  LT(_LOWER, KC_SPC)
 #define SPC_RSE  LT(_RAISE, KC_SPC)
 
 // Shift enter
@@ -42,21 +44,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
 		KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
 		KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_LPRN, _______, KC_RPRN, _______, _______, KC_PSCR, KC_HOME, KC_END,  KC_ENT, 
-		_______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______,_______,  KC_BSPC, _______, _______, _______, _______
+		_______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______, KC_BSPC, _______, _______, _______, _______
 	),
 	[_RAISE] = LAYOUT_ortho_5x15(
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSPC,
 		KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
 		KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
 		KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, KC_PSCR, KC_PGUP, KC_PGDN, KC_MPLY,
-		_______, _______, _______, _______, _______, _______, _______, _______, _______,_______,  _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
+		_______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______, KC_DEL,  KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
 	),
 	[_ADJUST] = LAYOUT_ortho_5x15(
 		KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6, _______, _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
 		_______, QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, 
-		_______, _______, _______, _______, _______, _______, _______, _______, _______,_______,  _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD
 	)
 };
 
